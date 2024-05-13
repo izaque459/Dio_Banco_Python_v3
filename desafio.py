c00171480b97a0e8165f185802dc1e582968247e
Isaac_Santos <isaacdsantos@hotmail.com>
2024-05-13 19:04:40 -0300

    Atualizado arquivo desafio com todas as classes que serao utilizadas

--- desafio.py
+++ desafio.py
@@ -1,4 +1,69 @@
 import textwrap
+from abc import ABC, abstractclassmethod, abstractproperty
+from datetime import datetime
+
+class Conta:
+    __saldo = None
+    __numero = None
+    __agencia = None
+    __cliente = None
+    __historico = None
+    
+    def __init__():
+        self.__saldo = 0.0
+        self.__numero = 0
+        self.__agencia = "000000"
+        self.__cliente = Cliente
+        self.__historico = Historico
+        
+    
+class ContaCorrente(Conta):
+    __limite = None
+    __limite_saques = None
+    
+    def __init__():
+        self.__limite = 0.0
+        self.__limite_saques = 1
+        
+class ContaPoupança(Conta):
+    __limite_saques = None
+    
+
+class Transacao(ABC):
+    def registrar_transacao(conta:Conta):
+        return None
+
+class Deposito(Transacao):
+    __valor = None
+    
+class Saque(Transacao): 
+    __valor = None
+    
+class Transferencia(Transacao):
+    __valor = None
+    
+class Historico:
+    def adicionar_transacao(transacao:Transacao):
+        return None
+
+class Cliente:
+    __endereco = None
+    __contas = None
+    
+    def __init__():
+        self.__endereco = "000000"
+        self.__contas = []
+
+
+class PessoaFisica(Cliente):
+    __cpf = None
+    __nome = None
+    __data_nascimento = None
+    
+class PessoaJuridica(Cliente):
+    __cnpj = None
+    __nome = None
+    __data_criacao = None
 
 class Main:
 	
@@ -7,6 +72,7 @@ class Main:
 		================ MENU ================
 		[d]\tDepositar
 		[s]\tSacar
+		[t]\tTransferir
 		[e]\tExtrato
 		[nc]\tNova conta
 		[lc]\tListar contas
